72c622090aaf1f5158d07370748b4f28ee9941cf
Ars Vladimirov <grokhael@gmail.com>
2023-09-25 09:48:05 +0000

    init calc

---
+++ CITATION.md
@@ -0,0 +1,19 @@
+If you make use of the MangoNet data, please cite the following reference:
+
+```bibtex
+@article{KESTUR201959,
+  title = {MangoNet: A deep semantic segmentation architecture for a method to detect and count mangoes in an open orchard},
+  journal = {Engineering Applications of Artificial Intelligence},
+  volume = {77},
+  pages = {59-69},
+  year = {2019},
+  issn = {0952-1976},
+  doi = {https://doi.org/10.1016/j.engappai.2018.09.011},
+  url = {https://www.sciencedirect.com/science/article/pii/S0952197618301970},
+  author = {Ramesh Kestur and Avadesh Meduri and Omkar Narasipura},
+  keywords = {Deep learning, Semantic segmentation, Object detection, Mango detection, Convolutional neural networks, Fully convolutional networks},
+  abstract = {This work presents a method for detection and counting of mangoes in RGB images for further yield estimation. The RGB images are acquired in open field conditions from a mango orchard in the pre-harvest stage. The proposed method uses MangoNet, a deep convolutional neural network based architecture for mango detection using semantic segmentation. Further, mango objects are detected in the semantic segmented output using contour based connected object detection. The MangoNet is trained using 11,096 image patches of size 200×200 obtained from 40 images. Testing was carried out on 1500 image patches generated from 4 test images. The results are analyzed for performance of segmentation and detection of mangoes. Results are analyzed using the precision, recall, F1 parameters derived from contingency matrix. Results demonstrate the robustness of detection for a multitude of factors such as scale, occlusion, distance and illumination conditions, characteristic to open field conditions. The performance of the MangoNet is compared with FCN variant architectures trained on the same data. MangoNet outperforms its variant architectures.}
+}
+```
+
+[Source](https://www.sciencedirect.com/science/article/abs/pii/S0952197618301970)
--- src/convert.py
+++ src/convert.py
@@ -1,63 +1,38 @@
-import supervisely as sly
-import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
-from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
-import shutil
+# https://github.com/avadesh02/MangoNet-Semantic-Dataset
 
+import os
+import shutil
+from urllib.parse import unquote, urlparse
+
+import numpy as np
+import supervisely as sly
+from cv2 import connectedComponents
+from dataset_tools.convert import unpack_if_archive
+from dotenv import load_dotenv
+from supervisely.io.fs import get_file_name, get_file_size
 from tqdm import tqdm
 
-def download_dataset(teamfiles_dir: str) -> str:
-    """Use it for large datasets to convert them on the instance"""
-    api = sly.Api.from_env()
-    team_id = sly.env.team_id()
-    storage_dir = sly.app.get_data_dir()
+import src.settings as s
 
-    if isinstance(s.DOWNLOAD_ORIGINAL_URL, str):
-        parsed_url = urlparse(s.DOWNLOAD_ORIGINAL_URL)
-        file_name_with_ext = os.path.basename(parsed_url.path)
-        file_name_with_ext = unquote(file_name_with_ext)
 
-        sly.logger.info(f"Start unpacking archive '{file_name_with_ext}'...")
-        local_path = os.path.join(storage_dir, file_name_with_ext)
-        teamfiles_path = os.path.join(teamfiles_dir, file_name_with_ext)
+def fix_masks(image_np: np.ndarray) -> np.ndarray:
+    lower_bound = np.array([70, 110, 0])
+    upper_bound = np.array([255, 255, 255])
+    condition_white = np.logical_and(
+        np.all(image_np >= lower_bound, axis=2), np.all(image_np <= upper_bound, axis=2)
+    )
 
-        fsize = api.file.get_directory_size(team_id, teamfiles_dir)
-        with tqdm(
-            desc=f"Downloading '{file_name_with_ext}' to buffer...",
-            total=fsize,
-            unit="B",
-            unit_scale=True,
-        ) as pbar:        
-            api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
-        dataset_path = unpack_if_archive(local_path)
+    lower_bound = np.array([0, 0, 0])
+    upper_bound = np.array([20, 20, 20])
+    condition_black = np.logical_and(
+        np.all(image_np >= lower_bound, axis=2), np.all(image_np <= upper_bound, axis=2)
+    )
 
-    if isinstance(s.DOWNLOAD_ORIGINAL_URL, dict):
-        for file_name_with_ext, url in s.DOWNLOAD_ORIGINAL_URL.items():
-            local_path = os.path.join(storage_dir, file_name_with_ext)
-            teamfiles_path = os.path.join(teamfiles_dir, file_name_with_ext)
+    image_np[np.where(condition_white)] = (255, 255, 255)
+    image_np[np.where(condition_black)] = (0, 0, 0)
 
-            if not os.path.exists(get_file_name(local_path)):
-                fsize = api.file.get_directory_size(team_id, teamfiles_dir)
-                with tqdm(
-                    desc=f"Downloading '{file_name_with_ext}' to buffer...",
-                    total=fsize,
-                    unit="B",
-                    unit_scale=True,
-                ) as pbar:
-                    api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
+    return image_np
 
-                sly.logger.info(f"Start unpacking archive '{file_name_with_ext}'...")
-                unpack_if_archive(local_path)
-            else:
-                sly.logger.info(
-                    f"Archive '{file_name_with_ext}' was already unpacked to '{os.path.join(storage_dir, get_file_name(file_name_with_ext))}'. Skipping..."
-                )
-
-        dataset_path = storage_dir
-    return dataset_path
-    
 def count_files(path, extension):
     count = 0
     for root, dirs, files in os.walk(path):
@@ -69,17 +44,69 @@ def count_files(path, extension):
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    # project_name = "MangoNet"
+    dataset_path = "/home/grokhi/rawdata/MangoNet-Semantic-Dataset/MangoNet Dataset"
+    ds_name = "ds"
+    batch_size = 3
+    images_folder_name = "original images"
+    masks_folder_name = "annotated images"
+    masks_ext = ".jpg"
+    masks_prefix = "Class_"
 
-    # ... some code here ...
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+    def create_ann(image_path):
+        labels = []
 
-    # return project
+        image_name = get_file_name(image_path)[5:]
+        mask_name = masks_prefix + image_name + masks_ext
+        mask_path = os.path.join(masks_pathes, mask_name)
+        ann_np = sly.imaging.image.read(mask_path)[:, :, :]
+        ann_np = fix_masks(ann_np)[:, :, 0]
+        img_height = ann_np.shape[0]
+        img_wight = ann_np.shape[1]
+        mask = ann_np != 0
+        ret, curr_mask = connectedComponents(mask.astype("uint8"), connectivity=8)
+        for i in range(1, ret):
+            obj_mask = curr_mask == i
+            curr_bitmap = sly.Bitmap(obj_mask)
+            if curr_bitmap.area > 100:
+                curr_label = sly.Label(curr_bitmap, obj_class)
+                labels.append(curr_label)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
+
+
+    obj_class = sly.ObjClass("mango", sly.Bitmap)
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(obj_classes=[obj_class])
+    api.project.update_meta(project.id, meta.to_json())
+
+    for ds_name in os.listdir(dataset_path):
+        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+        curr_ds_path = os.path.join(dataset_path, ds_name)
+
+        images_pathes = os.path.join(curr_ds_path, images_folder_name)
+        masks_pathes = os.path.join(curr_ds_path, masks_folder_name)
+        images_names = os.listdir(images_pathes)
+
+        progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
+
+        for img_names_batch in sly.batched(images_names, batch_size=batch_size):
+            images_pathes_batch = [
+                os.path.join(images_pathes, image_path) for image_path in img_names_batch
+            ]
+
+            anns_batch = [create_ann(image_path) for image_path in images_pathes_batch]
+
+            img_infos = api.image.upload_paths(dataset.id, img_names_batch, images_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
+
+            api.annotation.upload_anns(img_ids, anns_batch)
+
+            progress.iters_done_report(len(img_names_batch))
+    return project
 
 
